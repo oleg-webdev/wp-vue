8f46a2f836df84a01de7519823089a4796e1ea5a
nayami <cibulnicoleg@gmail.com>
2017-01-07 22:13:56 +0200

    auth step - 1

--- wp-content/themes/development-theme/AlicelfMaterial/ajax.user.php
+++ wp-content/themes/development-theme/AlicelfMaterial/ajax.user.php
@@ -1,1 +1,32 @@
 <?php
+/**
+ * ==================== Login ======================
+ * 07.01.2017
+ */
+add_action('wp_ajax_nopriv_ajx20174507084516', 'ajx20174507084516');
+add_action('wp_ajax_ajx20174507084516', 'ajx20174507084516');
+function ajx20174507084516()
+{
+	$response = [
+		'data'   => $_POST,
+		'status' => 'fail',
+	];
+	$user = get_user_by( 'email', $response['data']['login'] );
+	if(!$user) {
+		$response['status'] = 'Such user is not exists';
+		echo json_encode( $response );
+		die;
+	}
+
+	if(wp_check_password( $response['data']['pass'], $user->user_pass, $user->ID )) {
+		$response['status'] = 'success';
+		wp_set_auth_cookie($user->ID);
+		$response['user'] = am_user($user->ID);
+	} else {
+		$response['status'] = 'Password not matches';
+	}
+
+
+	echo json_encode( $response );
+	die;
+}
--- wp-content/themes/development-theme/script/dev/routes/components/authComponent.vue
+++ wp-content/themes/development-theme/script/dev/routes/components/authComponent.vue
@@ -1,9 +1,9 @@
 <template>
 	<div ref="authscope" id="Auth-scope">
 		<div class="container">
-			<div id="login-register-form" class="col-md-8 col-md-offset-2">
+			<div id="login-register-form"
+					 class="col-md-8 col-md-offset-2 animated rubberBand">
 				<br>
-
 				<md-card id="login-register-wrapper">
 
 					<md-button @click="toggleRegistration" title="Registration" class="md-fab"
@@ -22,14 +22,14 @@
 							</md-card-header>
 							<!--Login-->
 							<md-card-content id="am-loginform">
-								<form action="" method="post" role="form">
+								<form @submit.prevent="loginUser" action="" method="post" role="form">
 
 									<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-										<input class="mdl-textfield__input" type="text" id="am-username">
+										<input v-model="loginUserModel.login" class="mdl-textfield__input" type="text" id="am-username">
 										<label class="mdl-textfield__label" for="am-username">Username</label>
 									</div>
 									<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-										<input class="mdl-textfield__input" type="password" id="am-password">
+										<input v-model="loginUserModel.pass" class="mdl-textfield__input" type="password" id="am-password">
 										<label class="mdl-textfield__label" for="am-password">Password</label>
 									</div>
 
@@ -73,7 +73,8 @@
 									<md-card-actions>
 										<md-button class="md-raised md-accent animated"
 															 v-bind:class="{'fadeInUpBig':currentForm == 'registration'}"
-															 type="submit">Register</md-button>
+															 type="submit">Register
+										</md-button>
 									</md-card-actions>
 								</form>
 							</div>
@@ -90,27 +91,49 @@
 </template>
 
 <script>
+	import CurrentUser from '../../vuex/User'
 	export default {
-
 
 		data() {
 			return {
 
-				currentForm: 'login'
+				currentForm   : 'login',
+				loginUserModel: {
+					login: '',
+					pass : ''
+				},
 
+				errors: []
 			}
 		},
 
-		computed: {},
+		computed: {
+
+		},
 
 		methods: {
 
 			toggleRegistration(){
 				this.currentForm = this.currentForm === 'registration' ? 'login': 'registration';
+			},
+
+			loginUser() {
+				this.errors = []
+				const loginData = dataToPost('ajx20174507084516', this.loginUserModel);
+				this.$http.post(AMdefaults.ajaxurl, loginData).then(function(response) {
+					let data = JSON.parse(response.data);
+
+					if (data.status === 'success') {
+						CurrentUser.commit('setUserdata', data.user)
+						console.log(CurrentUser.state.userdata);
+						this.$router.push({name: 'userentrypoint'})
+					} else {
+						this.errors.push(data.status)
+					}
+				});
 			}
 
 		},
-
 
 		created(){
 
