825890203aaf6d86bdb72cffa6b69604c4feec0f
nayami <cibulnicoleg@gmail.com>
2017-04-25 12:37:34 +0300

    light theme title and favicon

--- wp-content/themes/light-theme/includes/template-actions.php
+++ wp-content/themes/light-theme/includes/template-actions.php
@@ -19,6 +19,36 @@ function aa_func_20172223122244()
 	echo $messages;
 }
 
+/**
+ * ==================== Change Title Defaults ======================
+ * 19.08.2016
+ */
+add_filter( 'wp_title', 'aa_func_20164019094058', 10, 1 );
+function aa_func_20164019094058( $title )
+{
+	if ( is_home() || is_front_page() ) {
+		$title = get_bloginfo( 'name' ) . " | " . get_bloginfo( 'description', 'display' );
+	} else if ( is_404() ) {
+		$title = get_bloginfo( 'name' ) . ' | .404!';
+	} else if ( is_search() ) {
+		$title = get_bloginfo( 'name' ) . " | Search " . get_search_query();
+	} else {
+		$title = get_the_title() . " | " . get_bloginfo( 'name' );
+	}
+
+	return $title;
+}
+
+//add_action( 'admin_head', 'aa_set_favicon' );
+//add_action( 'wp_head', 'aa_set_favicon' );
+function aa_set_favicon()
+{
+	$url = get_template_directory_uri().'/img/app-favicon.png';
+	$output = "<link rel='icon' type='image/png' href='{$url}'>";
+	echo $output;
+}
+
+
 add_filter( 'comment_form_fields', 'aa_func_20165127075134', 10, 1 );
 function aa_func_20165127075134( $fields )
 {
